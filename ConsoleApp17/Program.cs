a43ad059cca8f74c725e6fbd059d5d6ac648f348
lokes <lokes@LAPTOP-J2B9HTQV>
2022-01-12 12:36:07 +0530

    Add project files.

---
+++ ConsoleApp17/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ConsoleApp17
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var context = new EmployeeContext())
+            {
+                List<EmployeeDetails> employees = context.EmployeeDetails.ToList();
+                foreach(var employeeDetails in employees)
+                {
+                    Console.WriteLine(employeeDetails.Address);
+                }
+            }
+            
+        }
+    }
+}
